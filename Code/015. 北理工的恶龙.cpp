01286a4f0eb8440abdd999dfbad82f7fe710d13d
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-09-01 21:07:22 +0800

    Update and rename 015. 北理工的恶龙.c to 015. 北理工的恶龙.cpp

--- Code/015. 北理工的恶龙.cpp
+++ Code/015. 北理工的恶龙.cpp
@@ -14,3 +14,62 @@ Notes
 氪金3元，此时 A=5 ，打败第三条恶龙，此时 A=3 。
 最后直接打败第四条恶龙。
 */
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+#define N 200000
+long long int cash=0;
+int **XY=new int *[N];
+int xy[N],xi[N],yi[N];
+bool cmp1(int a[],int b[]){
+	if(a[0]!=b[0]) return a[0]<b[0];
+	if(a[1]!=b[1]) return a[1]<b[1];
+	if(a[2]!=b[2]) return a[2]<b[2];
+}
+bool cmp2(int a[],int b[]){
+	if(a[0]!=b[0]) return a[0]>b[0];
+	if(a[1]!=b[1]) return a[1]>b[1];
+	if(a[2]!=b[2]) return a[2]>b[2];
+}
+int main(){
+	long long int A=0;
+	int i,n,Xy,xY,x,y,pd=0;
+	scanf("%lld",&n);
+	Xy=0;
+	xY=n;
+	for(i=0;i<n;i++){
+		scanf("%d %d",&x,&y);
+		if(y<0){
+			xY-=1;
+			xy[xY]=x+y;
+			xi[xY]=x;
+			yi[xY]=y;
+			if(xy[xY]<xy[xY+1]&&xY!=0&&xY!=n-1){
+				pd=1;
+			}
+		}else{
+			xy[Xy]=x;
+			xi[Xy]=x;
+			yi[Xy]=y;
+			Xy+=1;
+		}
+	}
+	for(i=0;i<n;i++){
+		XY[i]=new int[3];
+		XY[i][0]=xy[i];
+		XY[i][1]=xi[i];
+		XY[i][2]=yi[i];
+	}
+	sort(XY,XY+Xy,cmp1);
+	if(pd==1)
+		sort(XY+xY,XY+n,cmp2);
+	for(i=0;i<n;i++){
+		if(A<XY[i][1]){
+			cash+=(XY[i][1]-A);
+			A+=(XY[i][1]-A)+XY[i][2];
+		}else{
+			A+=XY[i][2];
+		}
+	}
+	printf("%lld\n",cash);
+}
