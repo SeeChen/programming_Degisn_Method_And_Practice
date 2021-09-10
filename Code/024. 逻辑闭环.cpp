eb32b17a1fa847945dd05311a2733cbcb98ebff5
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-09-10 15:32:28 +0800

    None

--- Code/024. 逻辑闭环.cpp
+++ Code/024. 逻辑闭环.cpp
@@ -7,3 +7,52 @@ Input
 Output
 一行一个数，表示最少能够用前几条线索形成逻辑闭环完成推理。若无法完成输出-1。
 */
+#include <bits/stdc++.h>
+#define N 300005
+using namespace std;
+struct mark{
+	int event;
+	bool visit;
+	mark(int event_,bool visit_){
+		event=event_;
+		visit=visit_;
+	}
+};
+struct event{
+	public:
+		int eventA;
+		int eventB;
+};
+vector<mark> r[N];
+queue<int> q;
+bool bfs(int a,int b){
+	q.push(b);
+	while(!q.empty()){
+		int temp=q.front();
+		for(long i=0;i<r[temp].size();i++)
+			if(r[temp][i].visit==0){
+				if(r[temp][i].event==a)
+					return true;
+				r[temp][i].visit = 1;
+				q.push(r[temp][i].event);
+			}
+		q.pop();
+	}
+	return false;
+}
+int main(){
+	ios::sync_with_stdio(false);
+	int n,m;
+	scanf("%d %d",&n,&m);
+	for(int i=0;i<m;i++){
+		event temp;
+		scanf("%d %d",&temp.eventA,&temp.eventB);
+		r[temp.eventA].push_back((mark(temp.eventB,0)));
+		bool ans=bfs(temp.eventA,temp.eventB);
+		if(ans){
+			cout << i + 1 << endl;
+			return 0;
+		}
+	}
+	cout << -1 << endl;
+}
